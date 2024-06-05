b005bd6556817b5c64858a1c1e9f19ce8741bb92
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-05 19:59:00 +0530

    consume the API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 `use strict`;
 
-let count;
+let count = 5;
 const nasaAPIURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=${count}`;
+
+
+
+
+// consuming the API 
+
+const spaceFunction = async function(){
+    const data = await fetch(nasaAPIURL);
+    const response = await data.json();
+    console.log(response);
+}
+
+spaceFunction();
